10cb56cebecd7b741ab629b21fd3c3fbc0f52c20
Omar <omar.adel888@hotmail.com>
2019-11-01 06:11:22 +0200

    Initial Commit

---
+++ toDoList/toDo.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  toDoList
+//
+//  Created by z510 on 11/1/19.
+//  Copyright © 2019 z510. All rights reserved.
+//
+
+import UIKit
+
+class toDo: UITableViewController {
+    
+    let dolist = ["a","b","c"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "doCell")
+        cell.textLabel?.text = dolist[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+
+}
+
